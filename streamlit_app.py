ba2532022e0374db91e7f6f330eb1e30578bf3d5
Bayubahe56 <cyprienbayubahe56@gmail.com>
2025-05-07 18:19:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,12 @@ st.write("""
 # My first app
 Hello *world!*
 """)
+import streamlit as st
+import pandas as pd
+
+# Exemple : ton DataFrame
+df = pd.read_csv("Dadoseducationais/population.csv")
+
+# Affiche le DataFrame pour déboguer
+st.write(df)
 
